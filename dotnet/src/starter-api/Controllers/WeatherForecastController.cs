53ced142fd0e9d0a0ee8ed0e27b688b5b3656e0d
David Dobbins <david.dobbins@revunit.com>
2023-08-09 15:05:51 -0400

    add weather summary

--- dotnet/src/starter-api/Controllers/WeatherForecastController.cs
+++ dotnet/src/starter-api/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ public class WeatherForecastController : BaseController
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild","Perfect", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
